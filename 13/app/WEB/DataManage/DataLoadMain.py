e39288f98d3b77ce50005a609b1725c82662486e
KNS121 <komaroff.ns@gmail.com>
2025-11-16 01:10:52 +0300

    +main_data_clickhouse

--- 13/app/WEB/DataManage/DataLoadMain.py
+++ 13/app/WEB/DataManage/DataLoadMain.py
@@ -1,11 +1,15 @@
-import data_prep
-from Client_ClickHouse import client
-filepath = "test1.csv"
-if __name__ == "__main__":
+from . import data_prep
+from . Client_ClickHouse import client
+#filepath = "test1.csv"
+
+def data_main(filepath, num_collocation_points):
     data_manager = data_prep.DataManager(client, database='db')
-    success = data_manager.process_csv_data(filepath, num_collocation_points=300)
+    success = data_manager.process_csv_data(filepath, num_collocation_points)
 
     if success:
         print("Все операции выполнены успешно!")
     else:
         print("Произошла ошибка при обработке данных!")
+
+    return success
+
